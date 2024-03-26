19e36699d727ee54539e736865e515461c369ee0
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-26 17:40:41 +0100

    feat: add title and image

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+           message: `One Piece`,
+           img: `https://movieplayer.net-cdn.it/t/images/2023/08/09/one-piece-grear-5-teaser-promo_jpg_1600x900_crop_q85.jpg`
+        };
+    },
+    methods: {
+       
+    }
+}).mount('#app');
